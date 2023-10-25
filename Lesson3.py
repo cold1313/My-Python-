1b4990c9bf2101a2d93b7e2464f47f39f4fe321c
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-10-25 22:15:45 +0300

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,9 +1,21 @@
 num1 = int(input())
 num2 = int(input())
 num3 = int(input())
-if num1 != num2 and num2 != num3:
+if num1 != num2 and num2 != num3 and num3 != num10:
     print ("0")
 elif num1 == num2 and num2 == num3:
     print ("3")
 else:
     print ("2")  
+
+
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+if num1 == num3 or num2 == num4 or num2 == num3:
+    print ("YES")
+elif num1 != num3 and num2 != num4:
+    print ("NO")        

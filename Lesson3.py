8b50ec98101a22455c5b921726b28cf52c77487b
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-10-25 22:00:10 +0300

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,1 +1,9 @@
-print("hello, world!")
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+if num1 != num2 and num2 != num3:
+    print ("0")
+elif num1 == num2 and num2 == num3:
+    print ("3")
+else:
+    print ("2")  

2788a2ed7354b23742096c31635503c6767283ff
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-10-26 14:04:50 +0300

    Update and rename urok1 to urok1.py

---
+++ urok1.py
@@ -0,0 +1,49 @@
+n = int(input())
+sumofnum = 0 
+for i in range(n):
+    x = int(input())
+    sumofnum = sumofnum + x
+print(sumofnumw)    
+
+
+
+
+n = int(input()) 
+numof0 = 0
+for i in range(n):
+    x = int(input())
+    if x > 0:
+        numof0 = numof0 + 1
+print(numof0)
+
+
+
+
+a = int(input()) 
+b = int(input())
+if a > b:
+    for i in range(a, b - 1, -1):
+        print(i)
+if a < b:
+    for i in range(a, b + 1):
+        print (i)
+
+
+
+
+a = int(input()) 
+b = int(input())
+if a%2 != 0:
+    a = a + 1 
+for i in range (a , b+1, 2):
+        print(i)    
+
+
+
+a = int(input()) 
+multiplyofnum = 1
+for i in range(1 , a + 1):
+    multiplyofnum = multiplyofnum * i
+print(multiplyofnum)
+        
+        

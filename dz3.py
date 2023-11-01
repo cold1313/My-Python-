4bd90a033de463e1c397f6599badb5feb6bf8097
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-11-01 19:51:19 +0300

    Update dz3.py

--- dz3.py
+++ dz3.py
@@ -1,10 +1,42 @@
+
+n = int(input())
+numso = 0
+if n > 0: 
+    for i in range (1, 1/n, n+1): 
+        numso =  numso + 1/n 
+    print(numso) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 n = int(input())
 num1 = int
 num2 = int
 x = float(num1 , num2)
 for i in range (2*n):
     print(max(x)) !!!!!!!!!!!!!!!!!!!!!!!!!
-
 
 
 

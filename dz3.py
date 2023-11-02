6c02182ba89081777aa84d0752fc9e129dffaa01
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-11-02 13:14:28 +0300

    Update dz3.py

--- dz3.py
+++ dz3.py
@@ -1,42 +1,33 @@
 
 n = int(input())
-numso = 0
-if n > 0: 
-    for i in range (1, 1/n, n+1): 
-        numso =  numso + 1/n 
-    print(numso) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a, b = 1, 2  # a=1, b=2
+print(a, b)
+for i in range (n):
+    num1, num2 = map(int, input().split('; '))
+    print(min(num1 , num2))   минимум из н пар чисел 
 
 
 n = int(input())
-num1 = int
-num2 = int
-x = float(num1 , num2)
-for i in range (2*n):
-    print(max(x)) !!!!!!!!!!!!!!!!!!!!!!!!!
+sumofnum = 0
+for i in range (n):
+    sumofnum = sumofnum + i + 1
+print(sumofnum) сумма чисел от 1 до н (3 = 6) 
+
+
+n = int(input())
+sumofnum = 0
+for i in range (n):
+    sumofnum = sumofnum + 1/(i+1)
+print(sumofnum) (посчитайте сумму чисел от 1 + 1/2  до 1/н)
+
+
+
+
+
+
+
+
+
 
 
 

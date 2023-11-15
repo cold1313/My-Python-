bd06566d27dba0a4567b712032a1ada45a1b36cd
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-11-15 23:45:04 +0300

    Update dz5.py

--- dz5.py
+++ dz5.py
@@ -7,3 +7,13 @@ for i in range(2, n+1):
      prevnum, fibonachi = fibonachi, prevnum + fibonachi
 print(fibonachi)
 
+n = int(input())
+numofmore = 0
+while n != 0: 
+    nextn = int(input())
+    if nextn != 0 and nextn > n: 
+        numofmore = numofmore + 1
+    nextn = n
+print(numofmore)
+
+	Последовательность состоит из натуральных чисел и завершается числом 0. Определите, сколько элементов этой последовательности больше предыдущего элемента

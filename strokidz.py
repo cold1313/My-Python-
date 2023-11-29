1ee16e6d597b82beb4ad69b6e0af927122b1bed6
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-11-29 23:03:59 +0300

    Create strokidz.py

---
+++ strokidz.py
@@ -0,0 +1,13 @@
+s = input()
+for i in range(len(s)):
+    print(s[i]*(i+1))
+
+
+s = input()
+s1 = []
+numofunique = 0
+for i in s: 
+	if i not in s1: 
+		numofunique = numofunique + 1
+		s1.append(i)
+print(numofunique)

f74fec2efcfad7c28229c3752823057bbff34dd1
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-11-29 23:24:53 +0300

    Update strokidz.py

--- strokidz.py
+++ strokidz.py
@@ -11,3 +11,12 @@ for i in s:
 		numofunique = numofunique + 1
 		s1.append(i)
 print(numofunique)
+
+
+
+s = input()
+sumofnum = ''
+for i in s:
+    if i.isdigit():
+        sumofnum = sumofnum + i
+print(sumofnum)

6586e8473e749edbd4fdb4faf84cb7eae42a58ab
cold1313 <148446236+cold1313@users.noreply.github.com>
2023-12-13 11:18:44 +0300

    Create massivi.py

---
+++ massivi.py
@@ -0,0 +1,82 @@
+for i in range (10):
+    for j in range(10):
+        print((j+1) * (i+1), end = ' ')
+    print()
+(tablitsa umnozhenia)
+
+
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            print( 0 , end = ' ')
+        else:
+            print(1 , end = ' ')
+    print()
+диагональ квадрата. НА ДИАГОНАЛИ 0, ОСТАЛЬНОЕ 1
+
+
+
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i + j == n-1: 
+            print(1 , end = ' ')
+        else: 
+            print(0 , end = ' ')
+    print()
+  другая диагональ
+
+
+
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    a = []
+    for j in range(n):
+        if i == j:
+            a.append(0)
+        else:
+            a.append(1)
+    arr.append(a)
+for i in range (n):
+    for j in range (n):
+        print(arr[i][j] , end = ' ')
+    print()
+опять диагональ
+
+
+
+
+# print(arr.count(max(arr)))
+mmax = -1e9
+ans = 0
+for i in range(len(arr)):
+    if mmax < arr[i]:
+        mmax = arr[i]
+        ans = 1
+    elif mmax == arr[i]:
+        ans += 1
+
+
+# ans = 0
+# for i in arr:
+#     if i == mmax:
+#         ans += 1
+print(ans)n = int(input())
+arr = []
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+
+
+DZ!!!
+
+arr = list(map(int, input().split()))
+for i in range(0 , len(arr)):
+        if arr[i] > arr[i - 1]:
+            print(arr[i])
+
+

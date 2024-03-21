1e87898f33373f455c9946029b0313d14f2b3bb5
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-03-21 13:05:57 +0300

    Update init.py

--- povtorenie/init.py
+++ povtorenie/init.py
@@ -1,2 +1,17 @@
 arr = [int(x) for x in input().split()] #ввод массива целых чисел
 str = ''.join(arr) #преобразовние массива в строку
+
+
+задача на рекурсию
+def f(a):
+	if a == 1:
+		return 1
+	if a == 2:
+		return 2
+	if a > 2 and a%2 == 0:
+		return (3*a + f(a-3))/3
+	if a > 2 and a%2 != 0:
+		return (7*a + f(a-1) - f(a-2))/5
+
+print(f(35))
+

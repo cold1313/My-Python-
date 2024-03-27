fc12982d757bcd550a1be8916ef3b479a1506169
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-03-27 16:43:50 +0300

    Update init.py

--- povtorenie/init.py
+++ povtorenie/init.py
@@ -15,3 +15,43 @@ def f(a):
 
 print(f(35))
 
+
+
+for i in range (100, 1000): 
+    x = str(i)
+    k1 = int(x[0]) * int(x[1])
+    k2 = int(x[1]) * int(x[2])
+    if k1 > k2:
+        s1 = str(k1) + str(k2)
+    else:
+        s1 = str(k2) + str(k1)
+    if s1 == "123":
+        print(i)
+        break 
+
+
+for i in range(1, 100):
+    s = bin(i)[2:]  
+    s = str(s)
+    if s.count('1') > s.count('0'):
+        s += '1'
+    else:
+        s += '0'
+    r = int(s, 2)
+    if r > 80:
+        print(r)
+        break
+
+
+
+for i in range(1, 100):
+    s = bin(i)[2:]  
+    s = str(s)
+    if i % 2 == 0:
+        s += '00'
+    else:
+        s += '11'
+    r = int(s, 2) 
+    if r > 134:
+        print(i)
+        break

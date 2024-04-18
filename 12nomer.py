4454def4c21e8d7fb718420ae89d5faee85626ed
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-04-18 14:02:32 +0300

    Update 12nomer.py

--- 12nomer.py
+++ 12nomer.py
@@ -6,3 +6,25 @@ while "19" in s or "299" in s or "3999" in s:
 print(s) 
 
 
+
+
+
+arr = [0 for _ in range(1000)]
+for i in range(2, 1000):
+  if arr[i] == 0:
+    for j in range(2*i , 1000, i):
+      arr[j] = 1
+
+for i in range(41, 1000):
+        x = "0" + "1" * 40 + "2" * i + "0"
+        while "00" not in x:
+            x = x.replace('02' , '101' , 1)
+            x = x.replace('11' , '2' , 1)
+            x = x.replace('012' , '30' , 1)
+            x = x.replace('010' , '00' , 1)
+        sum = 0
+        for j in x:
+            sum += int(j)
+        if arr[sum] == 0:
+            print(i)
+            break

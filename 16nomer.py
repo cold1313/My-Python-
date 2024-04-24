6322d1d0d98e30a71b26d5bb4ade8fd7f402b665
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-04-24 14:31:40 +0300

    Update 16nomer.py

--- 16nomer.py
+++ 16nomer.py
@@ -22,3 +22,14 @@ for x in range(1 , 1000 + 1):
   if f(x) == 3:
     count += 1
 print(count)
+
+
+15 nomer
+def f(x):
+    if x == 1:
+      return 1
+    if x % 2 == 0:
+      return x + f(x-1)
+    if x > 1 and x % 2 != 0:
+      return 2 * f(x-2)
+print(f(26))

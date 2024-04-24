a020d6dfbde4915855e402f8462493c6b0bcdbbf
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-04-24 14:47:42 +0300

    Update 16nomer.py

--- 16nomer.py
+++ 16nomer.py
@@ -53,3 +53,14 @@ def f(x):
 print(f(6))
 
 
+nachalo resh 8 nomer:
+
+def f(x):
+  if x < 3:
+    return 1
+  if x > 2 and x % 2 != 0:
+    return f(x-1) + 3 * f(x-2)
+  if x > 2 and x % 2 == 0:
+    return 
+
+print(f(28))

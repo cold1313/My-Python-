016f32c8236b1c93e4c78ec8f6187bbbeee23739
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-04-25 13:33:14 +0300

    Update 16nomer.py

--- 16nomer.py
+++ 16nomer.py
@@ -69,3 +69,13 @@ def f(x):
 print(f(28))
 
 
+14 nomer dz:
+
+def f(x):
+    if x == 1:
+        return 1
+    if x == 2:
+        return 1
+    if x > 2:
+        return f(x-2) * (x-1)
+print(f(8))

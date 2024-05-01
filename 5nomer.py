158f951f2e35e82d1ae6c2e63ab4439cf4f19ad4
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-05-01 16:28:00 +0300

    Create 5nomer.py

---
+++ 5nomer.py
@@ -0,0 +1,16 @@
+for i in range (1 , 100000):
+    x = bin(i)[2:]
+    sum = 0
+    for j in range(len(x)):
+        sum += int(x[j])
+    x += str(sum%2)
+    second_sum = 0
+    for j in range(len(x)):
+        second_sum += int(x[j])
+    x += str(second_sum%2)
+    y = int(x , 2)
+    if y > 83:
+        print(y)
+        break
+
+

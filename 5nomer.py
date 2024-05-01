609205909f88c2e58f56e9d1b2d5be635b8f299d
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-05-01 16:46:02 +0300

    Update 5nomer.py

--- 5nomer.py
+++ 5nomer.py
@@ -14,3 +14,15 @@ for i in range (1 , 100000):
         break
 
 
+for i in range (1 , 10000):
+    x = bin(i)[2:]
+    x = str(x)
+    x += x[-1]
+    sum = 0
+    for j in range (len(x)):
+        sum += int(x[j])
+    x += str(sum%2)
+    y = int(x , 2)
+    if y > 105:
+        print(y)
+        break

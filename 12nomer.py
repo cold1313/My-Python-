3df16fb2ea083230419817a7014495342dc6bc38
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-05-02 12:20:37 +0300

    Update 12nomer.py

--- 12nomer.py
+++ 12nomer.py
@@ -28,3 +28,14 @@ for i in range(41, 1000):
         if arr[sum] == 0:
             print(i)
             break
+
+
+s = "1" + 80 *  '8'
+while "18" in s or "288" in s or "3888" in s:
+    if "18" in s:
+        s = s.replace("18" , "2" , 1)
+    elif "288" in s:
+        s = s.replace("288" , "3" , 1)
+    else:
+        s = s.replace("3888" , "1" , 1)
+print(s)

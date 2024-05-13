8114c68fec4e76877adcad319569165487c7ce68
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-05-13 13:53:02 +0300

    Update 12nomer.py

--- 12nomer.py
+++ 12nomer.py
@@ -72,3 +72,17 @@ for i in range(len(s)):
     if s[i] == "3":
         count_3 += 1
 print(count_1 , count_2 , count_3)
+
+
+for i in range(3 , 100001):
+    s = "5" + "2" * i
+    while "52" in s or "1122" in s or "2222" in s:
+        if "52" in s:
+            s = s.replace("52" , "1" , 1)
+        if "2222" in s:
+            s = s.replace("2222" , "5" , 1)
+        if "1122" in s:
+            s = s.replace("1122" , "25" , 1)
+    if 1*s.count("1")+2*s.count("2")+5*s.count("5") == 88:
+        print(i)
+        break

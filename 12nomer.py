0ddab259001d58bf88e4c39bc75028c9830a46bb
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-05-13 13:27:40 +0300

    Update 12nomer.py

--- 12nomer.py
+++ 12nomer.py
@@ -39,3 +39,15 @@ while "18" in s or "288" in s or "3888" in s:
     else:
         s = s.replace("3888" , "1" , 1)
 print(s)
+
+
+s = "0" + 12 * "1" + 15 * "2" + 17 * "3"
+while "01" in s or "02" in s or "03" in s:
+    s = s.replace("01" , "20" , 1)
+    s = s.replace("02" , "120" , 1)
+    s = s.replace("03" , '302' , 1)
+count = 0
+for i in range(len(s)):
+    if s[i] == "1":
+        count += 1
+print(count)

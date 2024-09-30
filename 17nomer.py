99820f09f04c86e790a00366cf4ef26b43b6f95b
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-09-30 13:22:05 +0300

    Update 17nomer.py

--- 17nomer.py
+++ 17nomer.py
@@ -12,11 +12,14 @@ print(max15)
 cnt = 0
 summ = 1e9
 for i in range(len(arr) - 2):
-    if (len(arr[i]) == 4 and len(arr[i + 1]) != 4 and len(arr[i + 2]) != 4
-            or len(arr[i]) != 4 and len(arr[i + 1]) == 4 and len(arr[i + 2]) != 4
-            or len(arr[i]) != 4 and len(arr[i + 1]) != 4 and len(arr[i + 2]) == 4):
-        if int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < max15:
-            cnt += 1
-            if int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < summ:
-                summ = int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2])
+    three = [arr[i], arr[i + 1], arr[i + 2]]
+    count4 = 0
+    for item in three:
+        if 999 < abs(int(item)) < 10000:
+            count4 += 1
+
+    if count4 == 1 and int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < max15:
+        cnt += 1
+        if int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < summ:
+            summ = int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2])
 print(cnt, summ)

d572c4451ce60e651a9c97bd8151a3f3b9ddcb65
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-09-30 13:13:30 +0300

    Update 17nomer.py

--- 17nomer.py
+++ 17nomer.py
@@ -1,1 +1,22 @@
+f = open('test.txt', 'r')
+# f = open('test.txt', 'w') - for writing objects into file
 
+arr = list(f.readlines())
+max15 = 0
+for i in range(len(arr)):
+    if abs(int(arr[i])) % 100 == 15:
+        if int(arr[i]) > max15:
+            max15 = int(arr[i])
+print(max15)
+
+cnt = 0
+summ = 1e9
+for i in range(len(arr) - 2):
+    if (len(arr[i]) == 4 and len(arr[i + 1]) != 4 and len(arr[i + 2]) != 4
+            or len(arr[i]) != 4 and len(arr[i + 1]) == 4 and len(arr[i + 2]) != 4
+            or len(arr[i]) != 4 and len(arr[i + 1]) != 4 and len(arr[i + 2]) == 4):
+        if int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < max15:
+            cnt += 1
+            if int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2]) < summ:
+                summ = int(arr[i]) + int(arr[i + 1]) + int(arr[i + 2])
+print(cnt, summ)

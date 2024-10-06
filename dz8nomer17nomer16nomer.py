a6a00646e6ce39bd79165ea6daacf5990e815f37
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-10-06 22:23:59 +0300

    Update and rename dz8nomer17nomer.py to dz8nomer17nomer16nomer.py

--- dz8nomer17nomer16nomer.py
+++ dz8nomer17nomer16nomer.py
@@ -34,3 +34,12 @@ for elem in arr:
     if (elem.count("X") == 1 and elem[4] == "X") or (elem.count("X") == 0):
         count += 1
 print(count)
+
+def f(x):
+    if x == 1:
+        return 1
+    if x == 2: 
+        return 3
+    if x > 2:
+        return f(x-1) * x + f(x-2) * (x-1)
+print(f(5))

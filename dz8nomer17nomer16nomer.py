84490395c04610559943c80008dfc83a66705707
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-10-07 11:17:44 +0300

    Update dz8nomer17nomer16nomer.py

--- dz8nomer17nomer16nomer.py
+++ dz8nomer17nomer16nomer.py
@@ -55,3 +55,13 @@ def f(x):
     if x > 2:
         return f(x-1) * x + f(x-2) * (x-1)
 print(f(5))
+
+
+def f(x):
+    if x == 1:
+        return 1
+    if x == 2:
+        return 1
+    if x > 2: 
+        return f(x-2) * x
+print(f(7))

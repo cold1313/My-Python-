7d0c1cf32881b2a86e287d290f621c0d7b13ae0c
cold1313 <148446236+cold1313@users.noreply.github.com>
2024-11-18 00:19:26 +0300

    Update AnnaGeorg.py

--- AnnaGeorg.py
+++ AnnaGeorg.py
@@ -39,3 +39,19 @@ for i in range(1 , 1000):
     if m == 2: 
         print(i)
         break
+
+
+for i in range(4 , 10000):
+    s = "8" + "7" * i
+    max = -10**9
+    while '57' in s or "877" in s or "777" in s:
+        if "57" in s:
+            s = s.replace("57" , "7" , 1)
+        if '877' in s:
+            s = s.replace('877' , "75" , 1)
+        if "777" in s: 
+            s = s.replace("777" , "8" , 1)
+        rez = 5 * s.count("5") * 7 * s.count("7") * 8 * s.count("8") 
+        if rez > max:
+            max = 5 * s.count("5") * 7 * s.count("7") * 8 * s.count("8")
+print(max)
